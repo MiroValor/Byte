e9fa14527fab57961cd80325a84daaae7ccf58a1
MiroValor <118856728+MiroValor@users.noreply.github.com>
2023-03-27 16:17:59 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+module.exports = {
+ "verification": "",
+ "description": "Pogy is a multi-purpose discord bot ready to skill up and boost up your Discord server!",
+ "domain": "https://mee6.saadukumidoriya.repl.co", // domain
+ "google_analitics": "", // google analitics
+ "token": process.env.TOKEN,
+ "https":"https://", // leave as is
+ "port":"5003",
+
+ "client_id":"993175062003851264", // bot client ID
+ "secret":"5GE4EgZNeWgcDnfLOmT-q3JdNQRYgxsv" // bot client secret for auth
+
+}
+
+
+// for dashboard, read more on https://github.com/IgorKowalczyk/majobot
